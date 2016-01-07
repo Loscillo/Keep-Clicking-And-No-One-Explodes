8b3913d2d91b9e30848cccd54c26241d2635a712
Nicolas GAILLARD <ngaillard.vosges@gmail.com>
2016-01-07 15:16:55 +0100

    [Update] Design of Knobs Module.

--- js/knobs_241.js
+++ js/knobs_241.js
@@ -1,47 +1,45 @@
-$(document).ready(function(){
-   for(var i=0;i<6;i++){
-    $('#modKnobs'+i).button();     
-   }
-   $('#modKnobsSolve').button();
-   $('#modKnobsReset').button();
-   
-});
-
-$("#modKnobsReset").click(function(){
-    $("#modKnobsForm")[0].reset();
-    $("#modKnobsOutput").html("&nbsp;");
-});
-
-$("#modKnobsSolve").click(function(){
-    console.log("YOO");
-    var answer = "Answer: ";
-    
-    var ledDB = {
-        '001001':"Up",
-        '001010':"Up",
-        '011001':"Down",
-        '000000':"Down",
-        '000010':"Left",
-        '101010':"Right",
-        '101000':"Right",
-    }
-    
-    var ledKeys = Object.keys(ledDB);
-    var ledConfig = "";
-    for(var i=0;i<6;i++){
-        if($('#modKnobs'+i).is(':checked')){
-            ledConfig+="1";
-        }else{
-            ledConfig+="0";
+$("#modKnobsReset").click(function () {
+    for (var i = 0; i < 6; i++) {
+        if ($('input[id=modKnobs' + i + ']:checked').val()) {
+            $('button[id=modKnobsButton' + i + ']').trigger("click");
         }
     }
-    console.log(ledConfig);
-    if(ledKeys.indexOf(ledConfig) === -1){
-        answer = "ERR - Input not in DB.";
-    }else{
-        answer+=ledDB[ledConfig];
+    $("#modKnobsError").hide();
+    $("#modKnobsSuccess").hide();
+    $(this).blur();
+});
+
+$("#modKnobsSolve").click(function () {
+    $(this).blur();
+
+    var ledDB = {
+        '001001': "Up",
+        '001010': "Up",
+        '011001': "Down",
+        '000000': "Down",
+        '000010': "Left",
+        '101010': "Right",
+        '101000': "Right"
+    };
+
+    var ledKeys = Object.keys(ledDB);
+    var ledConfig = "";
+    for (var i = 0; i < 6; i++) {
+        if ($('#modKnobs' + i).is(':checked')) {
+            ledConfig += "1";
+        }
+        else {
+            ledConfig += "0";
+        }
     }
 
-    $("#modKnobsOutput").html(answer);
-    return;     
+    if (ledKeys.indexOf(ledConfig) === -1) {
+        $("#modKnobsError").show();
+        $("#modKnobsSuccess").hide();
+    }
+    else {
+        $("#modKnobsAnswer").html(ledDB[ledConfig]);
+        $("#modKnobsSuccess").show();
+        $("#modKnobsError").hide();
+    }
 });
